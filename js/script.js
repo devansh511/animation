5b2adc56578f7963d3d484ed17c63258122608d3
Devansh Kumar <kumardevansh8@gmail.com>
2020-08-24 21:11:53 +0530

    Added three sections to the webpage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-function loading(){
+function loading() {
     
     var load = document.getElementById('preloader');
     
     load.style.display = "none";
 }
+
+var r_eye = document.getElementById('right-eye');
+
+var l_eye = document.getElementById('left-eye');
+
+var r_pupil = document.getElementById('right-pupil');
+
+var l_pupil = document.getElementById('left-pupil');
+
+var Face = document.getElementById('face');
+
+var Neck = document.getElementById('neck');
+
+var Mouth = document.getElementById('mouth');
