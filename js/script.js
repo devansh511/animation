cc383d0e3838891c25980f10180a5d63ca8ea6ac
Devansh Kumar <kumardevansh8@gmail.com>
2020-08-25 03:33:33 +0530

    Added keypress animation on the board

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
+// Preloader Function
+
 function loading() {
     
     var load = document.getElementById('preloader');
     
     load.style.display = "none";
 }
+
+// KeypressAnimation
+
+document.onkeydown = letter;
+
+function letter(event) {
+    
+    console.log(event);
+    var keycode = event.keyCode;
+    var Chars = String.fromCharCode(keycode);
+    var board = document.createElement('div');
+    board.id = 'letters';
+    var parentBoard = document.getElementsByTagName('div')[3];
+
+    parentBoard.appendChild(board);
+    board.style.position = 'absolute';
+
+    var randX = Math.round(Math.random() * parentBoard.offsetWidth);
+    var randY = Math.round(Math.random() * parentBoard.offsetHeight);
+
+    board.style.left = randX + 'px';
+    board.style.top = randY + 'px';
+    board.innerText = Chars;
+
+    board.style.transition = "all 0.5s linear 0s";
+    board.style.left = board.offsetLeft - 10 + 'px';
+    board.style.top= board.offsetTop - 10 + 'px';
+    board.style.fontSize = '50px';
+    board.style.opacity = 0;
+}
+
+// Human Animation
 
 var r_eye = document.getElementById('right-eye');
 
